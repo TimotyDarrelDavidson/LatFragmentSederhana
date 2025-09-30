92526da45e542842bdb3b0aa286a4d464902f56b
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2025-09-30 18:33:25 +0700

    MainActivity - membuat variabel ngereferensi 2 button - nge run fragment 1 di dalam MainActivity.kt - membuat fDua

--- app/src/main/java/test1/c14230235/latfragmentsederhana/MainActivity.kt
+++ app/src/main/java/test1/c14230235/latfragmentsederhana/MainActivity.kt
@@ -1,10 +1,12 @@
 package test1.c14230235.latfragmentsederhana
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var _btnFragmentSatu = findViewById<Button>(R.id.btnFragmentSatu)
+        var _btnFragmentDua = findViewById<Button>(R.id.btnFragmentDua)
+
+        if (savedInstanceState == null) replaceFragment(fsatu())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
