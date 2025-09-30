dcd1d56e1459dbc92d037f138431ce91883105ae
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2025-09-30 18:49:53 +0700

    Menambahkan onClickListener di Fragment 1

--- app/src/main/java/test1/c14230235/latfragmentsederhana/fsatu.kt
+++ app/src/main/java/test1/c14230235/latfragmentsederhana/fsatu.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,6 +32,21 @@ class fsatu : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val _btnOpenFSatu = view.findViewById<Button>(R.id.btnOpenFSatu)
+
+        _btnOpenFSatu.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            val fragmentDua = fDua()
+
+            fragmentTransaction.replace(
+                R.id.fragment_container,
+                fragmentDua
+            )
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onCreateView(
